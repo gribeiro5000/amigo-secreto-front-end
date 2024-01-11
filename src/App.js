cb795969da5ca6636dce2c44486266e92973327d
Gabriel Ribeiro <38739035+gribeiro5000@users.noreply.github.com>
2024-01-11 16:36:47 -0300

    Revert "Login form"

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-
-import "./App.css";
-import Grupos from "./components/pages/grupos.js";
-import Login from "./components/pages/login.js";
-import Home from "./components/pages/home.js";
-import ListaDeDesejos from "./components/pages/listaDeDesejos.js";
-import Cadastro from "./components/pages/cadastro.js";
-import InformacoesDoGrupo from "./components/pages/informacoesDoGrupo.js";
-import CriarGrupos from "./components/pages/criarGrupos.js";
-import MinhaConta from "./components/pages/minhaConta.js";
-import RecuperarSenha from "./components/pages/recuperarSenha.js";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="Cadastro" element={<Cadastro />} />
-        <Route path="Grupos" element={<Grupos />} />
-        <Route path="InformacoesDoGrupo" element={<InformacoesDoGrupo />} />
-        <Route path="CriarGrupos" element={<CriarGrupos />} />
-        <Route path="MinhaConta" element={<MinhaConta />} />
-        <Route path="RecuperarSenha" element={<RecuperarSenha />} />
-        <Route path="ListaDeDesejos" element={<ListaDeDesejos />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/components/formElements/buttonInput.js
+++
@@ -1,9 +0,0 @@
-function Button({text}) {
-  return (
-    <div>
-      <input type="button" value={text} />
-    </div>
-  );
-}
-
-export default Button;
